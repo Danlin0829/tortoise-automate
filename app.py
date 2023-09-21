11ead6b433aa56abec23b37acd32c879493317d0
Danlin0829 <1249866122@qq.com>
2023-09-21 19:42:44 +0800

    site init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, url_for, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index', methods=['POST'])
+def hello_world():  # put application's code here
+    # key = request.args.get("key")
+    # if key and not key.isspace():
+    #     return 'key is ' + key
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run()
